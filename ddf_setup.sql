ef94e297bfcdf3124165f8053fc220fb99c34837
Jakob Ladegaard Møller <jakob.moeller@non.se.com>
2024-05-24 17:00:59 +0200

    added ddf_setup

---
+++ ddf_setup.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS ddfs;
+
+CREATE TABLE ddfs (
+	id SERIAL PRIMARY KEY DEFAULT gen_random_uuid(),
+	device_type TEXT NOT NULL,
+	sku_number TEXT,
+	manufacturer TEXT NOT NULL,
+	model TEXT,
+	dce_serial UUID NOT NULL
+)
